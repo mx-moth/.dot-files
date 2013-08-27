21862397dee71c92084decf6ca9f35bcf81bca4c
Tim Heap <tim@ionata.com.au>
2013-08-28 07:39:38 +1000

    tmux: Respect the MPD_HOST environment variable in music.sh

--- tmux.d/music.sh
+++ tmux.d/music.sh
@@ -2,21 +2,28 @@
 
 source ~/.bashrc.d/colours.sh
 
+MPD_HOST=$( tmux show-environment 'MPD_HOST' )
+if $! ; then
+	MPC="mpc --host=${MPD_HOST#MPD_HOST=}"
+else
+	MPC="mpc"
+fi
+
 function exists? {
 	command -v "$@" 1>/dev/null 2>/dev/null
 }
 
 exists? mpc || exit 0
 
-out=$( mpc )
+out=$( $MPC )
 
 [[ $( echo "$out" | wc -l ) -eq 1 ]] && exit 0
 
-song=$( echo "$out" | head -n 1 )
+song=$( $MPC current --format='[%track%[-%disc%]: ][%title%[ - %album%][ - %artist%]' )
 play_info=$( echo "$out" | head -n 2 | tail -n 1)
 mpd_info=$( echo "$out" | tail -n 1)
 
-case $( echo "$play_info" | cut -d' ' -f1 ) in
+case "${play_info%% *}" in
 	'[playing]') left="❨"; right="❩" ;;
 	'[paused]')  left=" "; right=" " ;;
 esac
