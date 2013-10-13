9b76fcb6c32e6f2a86bc9898c31ec45e3c3ca99e
Tim Heap <tim@ionata.com.au>
2013-10-14 09:58:33 +1100

    bash: Improve ++venv
    It now searches all the way up to / or a file system boundary. It also
    prints the path of the virtualenv it activated.

--- bashrc.d/venv.sh
+++ bashrc.d/venv.sh
@@ -3,17 +3,33 @@
 
 # Quickly activate a venv in a standard location
 function ++venv() {
-	base="${1:-.}"
+	cwd=$( cd "$( readlink -e "$( pwd )" )" && pwd )
+	path="${1:-"$cwd"}"
 	locations=( 'venv' '.virthualenv' )
-	paths=( "$base" "$base/.." )
 
-	for path in "${paths[@]}" ; do
+	get_filesystem="df --output=target"
+	initial_filesystem="$( $get_filesystem "$path" )"
+
+	while ! [ -z "$path" ] ; do
+
 		for location in "${locations[@]}" ; do
 			if [[ -d $path/$location ]] ; then
+				echo "Activating $path/$location"
 				source $path/$location/bin/activate
 				return 0
 			fi
 		done
+
+		new_path=$( dirname "$path" )
+		if [ "$initial_filesystem" != "$( $get_filesystem "$new_path" )" ] ; then
+			echo  "Could not find venv to activate, crossed file system boundary at $path" >&2
+			return 2
+		fi
+		if [ "$path" == "/" ] && [ "$new_path" == "/" ] ; then
+			echo 'Could not find venv to activate' >&2
+			return 1
+		fi
+		path="$new_path"
 	done
 
 	echo 'Could not find venv to activate' >&2
