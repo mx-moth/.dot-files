9d6e8ff3a23a4a9c8d2a7b1610291a97c0ed86f2
Tim Heap <tim@ionata.com.au>
2014-04-16 12:13:55 +1000

    i3: Made the cwd-term script work from the shell
    Now it can be run as from the window manager, or from your current
    shell. Both will work and have the same result.

--- i3/cwd-term.sh
+++ i3/cwd-term.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 # i3 thread: https://faq.i3wm.org/question/150/how-to-launch-a-terminal-from-here/?answer=152#post-id-152
 
-CMD=${1:-x-terminal-emulator}
-shift 1
-CWD=''
 
-# Get window ID
-ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
+function cwdterm() {
+	CMD=${1:-x-terminal-emulator}
+	shift 1
 
-# Get PID of process whose window this is
-PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
+	CWD=''
+	# Get window ID
+	ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
 
-# Get last child process (shell, vim, etc)
-if [ -n "$PID" ]; then
-  TREE=$(pstree -lpnA $PID | tail -n 1)
-  PID=$(echo $TREE | awk -F'---' '{print $NF}' | sed -re 's/[^0-9]//g')
+	# Get PID of process whose window this is
+	PID=$(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3)
 
-  # If we find the working directory, run the command in that directory
-  if [ -e "/proc/$PID/cwd" ]; then
-    CWD=$(readlink /proc/$PID/cwd)
-  fi
-fi
+	# Get last child process (shell, vim, etc)
+	if [ -n "$PID" ]; then
+	  TREE=$(pstree -lpnA $PID | tail -n 1)
+	  PID=$(echo $TREE | awk -F'---' '{print $NF}' | sed -re 's/[^0-9]//g')
 
-if [ -n "$CWD" ]; then
-  cd $CWD && exec $CMD "$@"
-else
-  cd ~ && exec $CMD "$@"
-fi
+	  # If we find the working directory, run the command in that directory
+	  if [ -e "/proc/$PID/cwd" ]; then
+		CWD=$(readlink /proc/$PID/cwd)
+	  fi
+	fi
+
+	if [ -n "$CWD" ]; then
+	  cd $CWD && exec $CMD "$@" &
+	else
+	  cd ~ && exec $CMD "$@" &
+	fi
+}
+
+cwdterm "$@" 1>/dev/null 2>/dev/null </dev/null &
