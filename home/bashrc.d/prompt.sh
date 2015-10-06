9e2b3213a468bafd3f493276929679612162f1d4
Tim Heap <tim@timheap.me>
2015-10-06 11:05:26 +1100

    bash: filter some more boring processes in PS1

--- home/bashrc.d/prompt.sh
+++ home/bashrc.d/prompt.sh
@@ -8,7 +8,7 @@ export VCPROMPT_MODIFIED='*'
 export VCPROMPT_UNTRACKED='?'
 
 sep="╱"
-list=$( ppids  "$sep" "tmux" "urxvt" "init" "sh" "exo-helper-1" "x-terminal-emulator" "systemd")
+list=$( ppids  "$sep" "tmux" "urxvt" "init" "sh" "exo-helper-1" "x-terminal-emulator" "systemd" "xfsettingsd")
 if [ -n "$LC_SSH_PPLIST" ] && [[ "${list}" = sshd"${sep}"* ]] ; then
 	list="${LC_SSH_PPLIST}${sep}${list##*sshd${sep}}"
 fi
