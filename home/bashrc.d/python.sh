2748b78984f8ba0275e55a83294c780157139ef8
Tim Heap <tim@timheap.me>
2016-04-07 14:10:22 +0200

    bash: check pip exists before adding pip() function

--- home/bashrc.d/python.sh
+++ home/bashrc.d/python.sh
@@ -4,40 +4,35 @@
 # Settings for python shells
 export PYTHONSTARTUP=~/.pythonrc
 
-# Locally installed python packages
-local_python="$HOME/.local/lib/python2.7/site-packages"
-if [[ -d "$local_python" ]] && ! [[ "$PYTHONPATH" == *"$local_python"* ]] ; then
-	export PYTHONPATH="${PYTHONPATH:+$PYTHONPATH:}$local_python"
-fi
-unset local_python
-
 # Warn about using the global pip. This usually means we forgot to activate a
 # virtualenv
-system_pip=`env -i which pip`
-last_pip_time=0
-pip_cooldown=300 # five minutes
-function pip() {
-	current_pip=`which pip`
-	if [[ "$current_pip" == "$system_pip" ]] ; then
-		current_time="$( date +%s )"
-		if [[ "$(( $last_pip_time + $pip_cooldown ))" -le $current_time ]] ; then
-			echo "You are using the system-wide pip."
-			read -r -p "Are you sure you want to do this? [y/N] " response
-		else
-			response="y"
-		fi
+system_pip=`env -i which pip &2>/dev/null`
+if [[ $? -eq 0 ]] ; then
+	last_pip_time=0
+	pip_cooldown=300 # five minutes
+	function pip() {
+		current_pip=`which pip`
+		if [[ "$current_pip" == "$system_pip" ]] ; then
+			current_time="$( date +%s )"
+			if [[ "$(( $last_pip_time + $pip_cooldown ))" -le $current_time ]] ; then
+				echo "You are using the system-wide pip."
+				read -r -p "Are you sure you want to do this? [y/N] " response
+			else
+				response="y"
+			fi
 
-		case $response in
-			[yY])
-				$current_pip $@
-				last_pip_time=$current_time
-				;;
-			*) ;;
-		esac
-	else
-		$current_pip $@
-	fi
-}
+			case $response in
+				[yY])
+					$current_pip $@
+					last_pip_time=$current_time
+					;;
+				*) ;;
+			esac
+		else
+			$current_pip $@
+		fi
+	}
+fi
 
 # Make and source a virtualenv in the current directory
 function mkvenv.python() {
