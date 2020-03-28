c7cff60485cb3fc3fa358acbd5cec4ed25f2937b
Tim Heap <tim@timheap.me>
2020-03-28 13:41:39 +1100

    nvim: Add make-venv.sh script, only deals with python3

---
+++ config/nvim/make-venv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+HERE=$( cd $( dirname $0 ) && pwd )
+
+cd $HERE
+
+requirements=$HERE/requirements.txt
+
+venv3=$HERE/venv3
+[[ -e $venv3 ]] && rm -rf $venv3
+python3 -m venv $venv3
+$venv3/bin/pip install --upgrade pip
+$venv3/bin/pip install -r $requirements
