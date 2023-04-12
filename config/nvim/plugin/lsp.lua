9a879422ac8f0251cb4ef4447a0f90b206bbef7e
Tim Heap <tim.heap@csiro.au>
2023-04-12 14:48:45 +1000

    nvim: Update lsp settings

--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -40,19 +40,32 @@ end
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>tab split | lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>i', '<cmd>tab split | lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>tab split | lua vim.lsp.buf.declaration()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', opts)
+	local go_to_first = function(options)
+		if #(options.items) > 0 then
+			vim.cmd.tabnew(options.items[1].filename)
+			vim.fn['cursor'](options.items[1].lnum, options.items[1].col)
+		end
+	end
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '', {
+		noremap = true,
+		silent = true,
+		callback = function()
+			vim.lsp.buf.definition({on_list = go_to_first})
+		end
+	})
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>i', '<cmd>tab split | lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
 end
 
 require('lspconfig')['pylsp'].setup {
@@ -62,10 +75,13 @@ require('lspconfig')['pylsp'].setup {
 		pylsp = {
 			plugins = {
 				mccabe = { enabled = false },
+				pycodestyle = { enabled = false },
 				flake8 = {
+					executable = vim.g.python3_bin .. "/pflake8",
 					enabled = true,
 					ignore = { "W503", "E501", "C901" },
 				},
+				pyflakes = { enabled = false },
 			},
 		},
 	},
