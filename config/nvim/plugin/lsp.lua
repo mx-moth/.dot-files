0744c3d27c82b35042881352dbe4715f4a3fa348
Tim Heap <tim.heap@csiro.au>
2023-07-17 11:53:40 +1000

    nvim: improve lsp integration
    It now only activates various linting plugins if the requisite tools are
    installed in the projects virtual environment. This should mean that
    diagnostics for tools that are not relevant for a project will stop
    triggering false positives

--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -6,49 +6,19 @@ local callbacks = vim.lsp.callbacks
 local log = vim.lsp.log
 
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+-- Open diagnostic float with \d
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
 
-
--- Open location jumps (definition, implementation, etc) in a new tab
-local location_callback = function(_, method, result)
-  if result == nil or vim.tbl_isempty(result) then
-  local _ = log.info() and log.info(method, 'No location found')
-  return nil
-  end
-
-  api.nvim_command('tab split')
-
-  if vim.tbl_islist(result) then
-    util.jump_to_location(result[1])
-    if #result > 1 then
-      util.set_qflist(util.locations_to_items(result))
-      api.nvim_command("copen")
-      api.nvim_command("wincmd p")
-    end
-  else
-    util.jump_to_location(result)
-  end
-end
-
--- callbacks['textDocument/declaration']    = location_callback
--- callbacks['textDocument/definition']     = location_callback
--- callbacks['textDocument/typeDefinition'] = location_callback
--- callbacks['textDocument/implementation'] = location_callback
+-- Go to previous/next diagnostics with [d/]d
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>D', '<cmd>tab split | lua vim.lsp.buf.declaration()<CR>', opts)
-	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>d', '<cmd>tab split | lua vim.lsp.buf.definition()<CR>', opts)
 	local go_to_first = function(options)
 		if #(options.items) > 0 then
 			vim.cmd.tabnew(options.items[1].filename)
@@ -62,27 +32,100 @@ local on_attach = function(client, bufnr)
 			vim.lsp.buf.definition({on_list = go_to_first})
 		end
 	})
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>i', '<cmd>tab split | lua vim.lsp.buf.implementation()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>i', '<cmd>tab split | lua vim.lsp.buf.implementation()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>k', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader><C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	vim.keymap.set('n', '<leader>D', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', '<leader>K', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<leader>i', vim.lsp.buf.implementation, bufopts)
+
+	vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-require('lspconfig')['pylsp'].setup {
+
+-- Defaults for pylsp
+-- This disables all linting by default,
+-- specific linters will be enabled if they are found in the virtual environment.
+local pylsp_args = {
+	cmd = { vim.g.python3_bin .. "/pylsp", "-vv" },
 	on_attach = on_attach,
-	cmd = { vim.g.python3_bin .. "/pylsp" },
 	settings = {
 		pylsp = {
 			plugins = {
+				autopep8 = { enabled = false },
+				flake8 = { enabled = false },
+				pyls_isort = { enabled = false },
 				mccabe = { enabled = false },
+				pylsp_mypy = { enabled = false },
 				pycodestyle = { enabled = false },
-				flake8 = {
-					executable = vim.g.python3_bin .. "/pflake8",
-					enabled = true,
-					ignore = { "W503", "E501", "C901" },
-				},
 				pyflakes = { enabled = false },
+				yapf = { enabled = false },
 			},
 		},
 	},
 }
+
+local virtual_env = os.getenv('VIRTUAL_ENV')
+local venv = os.getenv('VENV_DIR')
+local in_virtual_env = virtual_env ~= nil or venv ~= nill
+
+-- If we are not in a virtual environment, activate ruff linting.
+-- If we are in a virtual environment,
+-- check which tools are available and activate them as necessary.
+if not in_virtual_env or vim.fn.executable('ruff') == 1 then
+	require('lspconfig').ruff_lsp.setup({
+		on_attach = on_attach,
+		cmd = { vim.g.python3_bin .. "/ruff-lsp" },
+		init_options = {
+			settings = {
+				args = {},
+			},
+		},
+	})
+end
+
+if in_virtual_env then
+	pylsp_plugins = pylsp_args['settings']['pylsp']['plugins']
+
+	if vim.fn.executable('flake8') == 1 then
+		pylsp_args['configurationSources'] = { 'flake8' }
+		pylsp_plugins['flake8'] = {
+			enabled = true,
+		}
+	else
+		-- Maybe configure the above things?
+	end
+
+	if vim.fn.executable('isort') == 1 then
+		pylsp_plugins['pyls_isort'] = {
+			enabled = true,
+		}
+	end
+
+	if vim.fn.executable('mypy') == 1 then
+		pylsp_plugins['pylsp_mypy'] = {
+			enabled = true,
+			dmypy = true,
+		}
+	end
+end
+
+require('lspconfig').pylsp.setup(pylsp_args)
--- config/nvim/plugin/python.vim
+++ config/nvim/plugin/python.vim
@@ -27,17 +27,3 @@ let g:jedi#show_call_signatures = 2
 let g:jedi#use_tabs_not_buffers = '1'
 let g:jedi#smart_auto_mappings = 0
 let g:jedi#force_py_version = 3
-
-" ALE config
-let g:ale_fixers = []
-for fixer in ['black', 'isort']
-	if executable(fixer)
-		call add(g:ale_fixers, fixer)
-	endif
-endfor
-
-" Use pflake8 if it is available. pflake8 draws settings from pyproject.toml
-if executable('pflake8')
-	let g:ale_python_flake8_executable = 'pflake8'
-	let g:ale_python_flake8_use_global = 1
-endif
